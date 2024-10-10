e7886ea642b835f68c3ded27a51ae2d689ae75c5
knezevicz <zarkoknezevic@lx-media.at>
2024-10-10 16:54:10 +0200

    added tours page RecommendedTour list

--- storyblok-nextjs-demo/src/app/tours/page.tsx
+++ storyblok-nextjs-demo/src/app/tours/page.tsx
@@ -1,9 +1,36 @@
-const ToursPage = () => {
-    return (
-        <div>
-            Tours page
-        </div>
-    );
+import { getStoryblokApi, StoryblokStory } from "@storyblok/react/rsc";
+import {RecommendedTour} from "@/components/RecommendedTour";
+
+const fetchToursPage = async () => {
+  const client = getStoryblokApi();
+  const response = await client.getStory(`tours`, {
+    version: "draft",
+    resolve_relations: "recommended_tours.tours"
+  });
+  return response.data.story;
+};
+
+const fetchAllTours = async () => {
+  const client = getStoryblokApi();
+  const response = await client.getStories({
+    content_type: "tour",
+    version: "draft",
+  });
+  return response.data.stories;
+};
+
+const ToursPage =  async ()=> {
+  const story = await fetchToursPage();
+  const tours = await fetchAllTours();
+
+  return (
+    <div>
+      <StoryblokStory story={story} bridgeOptions={[]}/>
+      {tours.map((tour: any) => (
+        <RecommendedTour story={tour} key={tour.content._uid}/>
+      ))}
+    </div>
+  );
 };
 
 export default ToursPage;
