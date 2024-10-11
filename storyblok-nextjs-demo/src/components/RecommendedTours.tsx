c806d6414e91ecc98d1944fc585815e2a438cc2f
knezevicz <zarkoknezevic@lx-media.at>
2024-10-12 01:16:49 +0200

    fix for static pages

--- storyblok-nextjs-demo/src/components/RecommendedTours.tsx
+++ storyblok-nextjs-demo/src/components/RecommendedTours.tsx
@@ -4,7 +4,7 @@ export const RecommendedTours = (props: any) => {
   return (
     <section className="container w-full mx-auto py-16 px-4">
       <h2 className="text-3xl md:text-4xl font-bold text-center">{props.blok.headline}</h2>
-      <div className="grid md:grid-cols-2 gap-8 mt-16">
+      <div className="grid md:grid-cols-2 gap-6 mt-16">
         {props.blok.tours.map((tour: any) => (
           <RecommendedTour story={tour} key={tour.content._uid} />
         ))}
