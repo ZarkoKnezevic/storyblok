38b8e79538a8ed461b35c456f17cf4fbebb7f26b
knezevicz <zarkoknezevic@lx-media.at>
2024-10-14 20:28:37 +0200

    added custom getStoryblokApi function and block implementation

--- storyblok-nextjs-demo/src/utils/storyblok.tsx
+++ storyblok-nextjs-demo/src/utils/storyblok.tsx
@@ -1,9 +1,4 @@
-import {
-  StoryblokClient,
-  apiPlugin,
-  getStoryblokApi as getStoryblokApiDefault,
-  storyblokInit,
-} from "@storyblok/react/rsc";
+import { storyblokInit, apiPlugin, StoryblokClient, getStoryblokApi as getStoryblokApiDefault } from "@storyblok/react/rsc";
 
 let storyblokApi: StoryblokClient | undefined = undefined;
 
@@ -13,18 +8,31 @@ export const AppStoryblokInit = (options?: {
 }) => {
   if (!storyblokApi) {
     storyblokInit({
-      accessToken: process.env.STORYBLOK_TOKEN,
+      accessToken: process.env.STORYBLOK_TOKEN as string, // Ensure token is passed correctly
       use: [apiPlugin],
       components: options?.components || {},
-      enableFallbackComponent: options?.enableFallbackComponent || false,
+      apiOptions: {
+        fetch: (input: any, init?: any): Promise<Response> => {
+          const cacheVersion = Date.now(); // Generate the cache version dynamically
+
+          const url = new URL(input);
+          url.searchParams.set("cv", cacheVersion.toString()); // Set the cache version
+
+          return fetch(url.toString(), {
+            ...init,
+            cache: process.env.NODE_ENV === "development" ? "no-store" : "force-cache",
+          });
+        },
+      },
     });
 
-    storyblokApi = getStoryblokApiDefault();
+    storyblokApi = getStoryblokApiDefault(); // Initialize Storyblok API client
   }
 
   return storyblokApi;
 };
 
+// Function to get the Storyblok API client
 export const getStoryblokApi = (): StoryblokClient => {
-  return AppStoryblokInit();
+  return AppStoryblokInit(); // Initialize before getting the API client
 };
